1b7b2176ffed3c7a9c34dc780433ec4538f4b3be
Leif Denby <leif@denby.eu>
2025-11-27 22:51:56 +0100

    rename package mlcast_dataset_validator

--- mlcast_dataset_validator/__init__.py
+++ mlcast_dataset_validator/__init__.py
@@ -5,6 +5,6 @@ from importlib.metadata import PackageNotFoundError, version
 __all__ = ["__version__"]
 
 try:
-    __version__ = version("mlcast-validator")
+    __version__ = version("mlcast-dataset-validator")
 except PackageNotFoundError:  # pragma: no cover - package metadata unavailable
     __version__ = "0.0.0"
